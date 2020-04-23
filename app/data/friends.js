097f21334cda2d6d62bee7ba2186a1e5d0bae476
claire-labry <clairestjlabry@gmail.com>
2020-04-23 18:36:34 -0500

    adding the latest files

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+var friends =[
+    {
+        'name': 'Willis',
+        'photo': 'placeholder',
+        'scores':[
+            1,
+            3,
+            5,
+            2,
+            3,
+            1,
+            4,
+            5,
+            1,
+            2
+        ]
+    },
+    {
+        'name': 'Charles',
+        'photo': 'placeholder',
+        'scores':[
+            1,
+            3,
+            5,
+            2,
+            3,
+            1,
+            4,
+            5,
+            1,
+            2
+        ]
+    },
+
+]
---
+++ server.js
@@ -0,0 +1,10 @@
+// Dependencies 
+
+var express = require('express');
+var path = require('path');
+
+// Sets + runs the express app
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
