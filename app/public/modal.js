58bc932ea69b81f23ef1425e69d10cae7bcde73d
claire-labry <clairestjlabry@gmail.com>
2020-06-09 16:13:12 -0500

    fixed images to be media responsive, cleaned up API links and formatting on survey page

--- app/public/modal.js
+++ app/public/modal.js
@@ -11,52 +11,52 @@ closeBtn.addEventListener('click', closeModal);
 window.addEventListener('click', clickOutside);
 
 function openModal() {
-    modal.style.display = 'block';
+  modal.style.display = 'block';
 }
 
 function closeModal() {
-    modal.style.display = 'none';
+  modal.style.display = 'none';
 }
 
 function clickOutside(e) {
-    if (e.target == modal) {
-        modal.style.display = 'none';
-    }
+  if (e.target == modal) {
+    modal.style.display = 'none';
+  }
 }
 
 $('#modalBtn').on('click', function (e) {
-    event.preventDefault();
+  event.preventDefault();
 
-    var surveyValidation = true;
+  var surveyValidation = true;
 
-    if ($('#fname').val() === '' || $('photo').val() === '') {
-        surveyValidation = false;
-    }
-    if (surveyValidation === true) {
-        var user = {
-            name: $('#fname').val().trim(),
-            photo: $('#photo').val().trim(),
-            scores: [
-                $('#match0').val(),
-                $('#match1').val(),
-                $('#match2').val(),
-                $('#match3').val(),
-                $('#match4').val(),
-                $('#match5').val(),
-                $('#match6').val(),
-                $('#match7').val(),
-                $('#match8').val(),
-                $('#match9').val(),
-            ]
-        };
+  if ($('#fname').val() === '' || $('photo').val() === '') {
+    surveyValidation = false;
+  }
+  if (surveyValidation === true) {
+    var user = {
+      name: $('#fname').val().trim(),
+      photo: $('#photo').val().trim(),
+      scores: [
+        $('#match0').val(),
+        $('#match1').val(),
+        $('#match2').val(),
+        $('#match3').val(),
+        $('#match4').val(),
+        $('#match5').val(),
+        $('#match6').val(),
+        $('#match7').val(),
+        $('#match8').val(),
+        $('#match9').val(),
+      ],
+    };
 
-        var currentURL = window.location.origin;
+    var currentURL = window.location.origin;
 
-        $.post(currentURL + '/api/friends', user, function (data) {
-            $('#newFriend').text(data.name);
-            $('#newFriendPhoto').attr('src', data.photo);
-        });
-    } else {
-        alert('Complete is Incomplete! Don\'t you want a new friend?')
-    }
+    $.post(currentURL + '/api/friends', user, function (data) {
+      $('#newFriend').text(data.name);
+      $('#newFriendPhoto').attr('src', data.photo);
+    });
+  } else {
+    alert("Complete is Incomplete! Don't you want a new friend?");
+  }
 });
