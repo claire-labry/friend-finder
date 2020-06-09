7ae3bb0bda01fd0af1e513e6677cde063f01ce8c
claire-labry <clairestjlabry@gmail.com>
2020-06-09 16:17:21 -0500

    added my comments to server.js file for improvements for future deployment

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-// Dependencies 
+// Dependencies
 
 var express = require('express');
-var path = require ('path');
+var path = require('path');
 // Sets + runs the express app
 var app = express();
 
-// Creates the initial PORT 
+// Creates the initial PORT
 var PORT = process.env.PORT || 3000;
 
 // To enable Express to handle data parsing
@@ -15,11 +15,18 @@ app.use(express.static('app/public'));
 
 // Routes for Friend Finder
 
-require("./app/routes/apiRoutes")(app);
-require("./app/routes/htmlRoutes")(app);
+require('./app/routes/apiRoutes')(app);
+require('./app/routes/htmlRoutes')(app);
 
 // PORT Listener
 
-app.listen(PORT, function(){
-    console.log('App is listening on PORT: ' + PORT);
-})
+app.listen(PORT, function () {
+  console.log('App is listening on PORT: ' + PORT);
+});
+
+// Improvements for Future Deployment
+// Ensure that user can upload their images when take survey
+// Have user's photo show up next to new friend
+// hardcode more new friends to data for matching
+// Add 'Home' button to modal
+// Turn this app into a full MERN application
